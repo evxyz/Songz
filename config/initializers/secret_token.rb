230ae68f411474820c18ee75d5c3eddb633a71e0
evx001 <eviexeris@yahoo.com>
2012-05-23 19:30:38 -0400

    1st Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songz::Application.config.secret_token = 'e6d809e35db1b544140788ad45c3200feb10b366cd50e6707d103f5989592efe66cabb2a28a6d9608ac0c169b2f37a5bc523d5c1232c0f0091cdaf126bb88129'
